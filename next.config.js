f8cba8558bd571532f01fd0e468d5c8b515b7c7f
maru0210 <m.ryuto0210@gmail.com>
2023-05-02 23:34:02 +0900

    fix a error : export-image-api

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    domains: ['localhost'],
-  }
+    unoptimized: true,
+  },
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
